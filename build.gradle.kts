861807f03d0007f4e190541b04758ae5d22b2416
Jonathan McDonnell <jonathan.code@atomicmail.io>
2026-06-01 15:19:39 -0400

    upgrade spring boot, ignore lsp db

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.9"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.32"
-  	kotlin("plugin.spring") version "1.5.32"
+	id("org.springframework.boot") version "3.5.0"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version "2.1.21"
+  	kotlin("plugin.spring") version "2.1.21"
 }
 
 group = "store.somethingbaked"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+kotlin {
+    jvmToolchain(21)
+}
 
 repositories {
 	mavenCentral()
@@ -21,7 +24,6 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
@@ -34,8 +36,7 @@ tasks.withType<Test> {
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
+    }
 }
