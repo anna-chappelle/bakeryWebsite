9f5ac9d4712844ed43715d43f8fe7587691739df
Jonathan McDonnell <jonathan.code@atomicmail.io>
2026-06-01 20:36:28 -0400

    change testing controller endpoint

--- src/main/kotlin/store/somethingbaked/Controller.kt
+++ src/main/kotlin/store/somethingbaked/Controller.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 @RestController
 class MyController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     fun test(): String {
         return "{}"
     }
